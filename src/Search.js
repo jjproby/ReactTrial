da2411328d1a46151f07a579e0715a5bb8d1804d
Jordan Proby <jproby19@csl14-l.cornellcollege.edu>
2018-10-01 10:09:20 -0500

    test search

--- src/Search.js
+++ src/Search.js
@@ -8,11 +8,16 @@ class Search extends Component {
     };
   }
 
+  updateSearch(event) {
+    this.setState({search: event.target.value.substr(0,3)});
+  }
+
   render() {
     return (
       <div>
       <input type="text"
-          value={this.state.search} />
+          value={this.state.search}
+          onChange = {this.updateSearch.bind(this)}/>
       </div>
     )
   }
