bc5f26e5d6d19974f4291aa059b0d654fd527663
jjproby <jjproby719@gmail.com>
2018-10-08 10:44:38 -0500

    Got pokedex entries work. I'm proud of myself. Yay!

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ const Wrapper = styled.section`
   background: papayawhip;
 `;
 
+const Section = styled.p`
+  font-size: 1em;
+  font-weight: bold;
+`;
+
 class App extends Component {
   constructor() {
     super();
@@ -18,6 +23,7 @@ class App extends Component {
     this.state = {
       query: '',
       data: [],
+      data2: [],
       error: '',
     };
 
@@ -36,26 +42,46 @@ class App extends Component {
               data: json,
               error: json.Error,
           }, () => {
-            console.log(query);
+            //console.log(query);
             //console.log(json);
           });
         }).catch(err => this.setState({
             error: 'Error Occurred: Try Again',
             data: [],
         }));
-  }
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${query}/`)
+      .then(res => res.json())
+      .then(res => {
+          return res;
+      }).then(json => {
+        this.setState({
+            query: query,
+            data2: json,
+            error: json.Error,
+        }, () => {
+          console.log(query);
+          //console.log(json);
+        });
+      }).catch(err => this.setState({
+          error: 'Error Occurred: Try Again',
+          data: [],
+  }));
+}
 
   render () {
     console.log(this.state.data)
+    console.log(this.state.data2)
     //console.log(this.state.data.abilities)
     const abilities = this.state.data.abilities
     const games = this.state.data.game_indices
+    const pokeData = this.state.data2.flavor_text_entries
 
     let abilitiesList = [];
     let sprite = null;
     let shinysprite = null;
     let gameList = [];
     let button = null;
+    let pokedex = [];
 
     const hideThing = () => {
       if (document.getElementById('sprite1').hidden === false) {
@@ -78,6 +104,16 @@ class App extends Component {
       button = <button type="button" onClick={hideThing}> Change Color </button>
     }
 
+    if (pokeData !== undefined) {
+      pokedex = this.state.data2.flavor_text_entries.map((entry, i) => {
+        if (entry.language.name === "en") {
+          return (
+            <li key={i}>{entry.version.name} : {entry.flavor_text}</li>
+          )
+        }
+      })
+    }
+
     if (games !== undefined) {
       gameList = this.state.data.game_indices.map((game, i) => {
         return (
@@ -92,17 +128,21 @@ class App extends Component {
           <Title> SEARCH FOR POKEMON </Title>
           <h2> Input a number 1 - 802 </h2>
           <Search search={this.search}/>
-          <p> name : {this.state.data.name}</p>
-          <p> id : {this.state.data.id}</p>
-          <p> height : {this.state.data.height}</p>
-          <p> weight : {this.state.data.weight}</p>
-          <p> abilities: </p>
+          <p> Name : {this.state.data.name}</p>
+          <p> ID : {this.state.data.id}</p>
+          <p> Height : {this.state.data.height}</p>
+          <p> Weight : {this.state.data.weight}</p>
+          <Section> Abilities: </Section>
           <ul>
             {abilitiesList}
           </ul>
-          <p> games: </p>
+          <Section> Games: </Section>
           <ul>
             {gameList}
+          </ul>
+          <Section> Pokedex Entries: </Section>
+          <ul>
+            {pokedex}
           </ul>
           <p id="sprite1"> {sprite}
           <br /> Default</p>
