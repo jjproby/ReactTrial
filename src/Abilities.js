b95b0c2f9938d6a3188bf33837a36a337d0855ed
jjproby <jjproby719@gmail.com>
2018-10-09 15:15:22 -0500

    Abilites tab now does things. Yay! It is working, but it neeeds mroe things to be even better!

--- src/Abilities.js
+++ src/Abilities.js
@@ -1,13 +1,77 @@
 import React, { Component } from "react";
+import Search from './Search';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: #5995ea;
+  outline: 5px solid black;
+`;
 
 class Abilities extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      query: '',
+      data: [],
+      error: '',
+    };
+
+    this.search = this.search.bind(this);
+  }
+
+  search({query}) {
+    fetch(`https://pokeapi.co/api/v2/ability/${query}/`)
+        .then(res => res.json())
+        .then(res => {
+            return res;
+        }).then(json => {
+          this.setState({
+              query: query,
+              data: json,
+              error: json.Error,
+          }, () => {
+            //console.log(query);
+            //console.log(json);
+          });
+        }).catch(err => this.setState({
+            error: 'Error Occurred: Try Again',
+            data: [],
+        }));
+      }
 
   render() {
+    console.log(this.state.data)
+
+    const abilityEffects = this.state.data.effect_entries
+
+    let effects = [];
+
+    if (abilityEffects !== undefined) {
+      if (abilityEffects !== undefined) {
+        effects = this.state.data.effect_entries.map((effect, i) => {
+          if (effect.language.name === "en") {
+            return (
+              <p key={i}> Effect : {effect.effect}</p>
+            )
+          }
+        })
+      }
+    }
+
     return (
-      <div>
-        <h2> YOU DID IT </h2>
-        <p>You got to a different page</p>
-      </div>
+      <Wrapper>
+        <h2> Search for ability </h2>
+        <Search search = {this.search}/>
+        <p> Name : {this.state.data.name} </p>
+        {effects[0]}
+        {
+          this.state.error
+          ? <p>{this.state.error}</p>
+          : null
+        }
+      </Wrapper>
     );
   }
 }
--- src/Main.js
+++ src/Main.js
@@ -11,7 +11,7 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <h1> Simple SPA </h1>
+        <h1> MY THING </h1>
         <HashRouter>
           <div className="outline">
             <ul className="header">
